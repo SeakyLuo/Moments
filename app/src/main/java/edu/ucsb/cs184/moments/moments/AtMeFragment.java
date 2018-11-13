bdf7382ec29aa5fd5b1fa2a63cadf8576799a680
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-11-12 23:22:29 -0800

    Save work

---
+++ app/src/main/java/edu/ucsb/cs184/moments/moments/AtMeFragment.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs184.moments.moments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AtMeFragment extends Fragment {
+
+    private Context context;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_atme, container, false);
+        context = getContext();
+        return view;
+    }
+}
